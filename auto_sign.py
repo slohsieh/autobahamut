dc040d4ed0ee805b60be8f27e6a4a5e9d9de8947
slohsieh <slohsieh@gmail.com>
2025-03-26 22:03:51 +0800

    correct

--- auto_sign.py
+++ auto_sign.py
@@ -9,7 +9,7 @@ def sign_in(sess: requests.Session):
     if sign_info['data']['signin'] == 1:
         return
     token = sess.get('https://www.gamer.com.tw/ajax/get_csrf_token.php').text
-    json_info = sess.post(__SIGN_IN_URL__,, data={'action': '1', 'token': token})
+    json_info = sess.post(__SIGN_IN_URL__, data={'action': '1', 'token': token})
     json_info = json_info.json()
     if 'data' in json_info:
         print(f'巴哈姆特自動簽到成功!!\n已簽到第 {str(json_info["data"]["days"])} 天')
